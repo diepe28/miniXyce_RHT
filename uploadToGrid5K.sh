59d170042fd008fc1c8d179c4ef3ffc5236ef81b
diego <diepe2892@gmail.com>
2018-05-16 12:00:31 -0600

    Updating sh commands to run with cirBig.net insteads of cirHuge.net

--- uploadToGrid5K.sh
+++ uploadToGrid5K.sh
@@ -63,7 +63,12 @@ echo "Success!!"
 #rm -rf nantes/public/miniXyce_RHT/ && rm -rf nancy/public/miniXyce_RHT/ && rm -rf lyon/public/miniXyce_RHT
 
 # Inside a node (if tar.gz was copied into public/ with correct file structure), this will compile everything
+# If name of folder is miniXyce_RHT (my machine)
 #cd public/ && tar -xzvf miniXyce_RHT.tar.gz && rm miniXyce_RHT.tar.gz && cd miniXyce_RHT/cmake-build-debug/ && cmake .. && make
+
+# If name of folder is miniXyce_RHT-master (directly from git)
+#cd public/ && tar -xzvf miniXyce_RHT-master.tar.gz && rm miniXyce_RHT-master.tar.gz && cd miniXyce_RHT-master/cmake-build-debug/ && cmake .. && make
+
 
 # Asking for nodes, remember to kill job when disconected with oardel jobId
 
@@ -79,7 +84,7 @@ echo "Success!!"
 #lyon
 #oarsub -p "cluster='nova'" -I -l nodes=1,walltime=5
 
-# These are the commands for lyon/nova, 
+# These are the commands for lyon/nova,
 
 #Lyon - Nova 0,16 2,18 4,20... are hyperThreads (lstopo result)
 #Core0       Core1     Core2        Core3     Core4       Core5    Core6      Core7
@@ -88,44 +93,48 @@ echo "Success!!"
 #Core8       Core9     Core10       Core11    Core12      Core13   Core14     Core15
 #1,17        3,19      5,21         7,23      9,25        11,27    13,29      15,31
 
-# this as the commands, must copy and paste 
+# this as the commands, must copy and paste
 if [ 1 -eq 0 ]; then
+# there are these the test files, in ascending order
+# cirBig.net
+# cirHugeSmall.net 
+# cirHuge.net 
+
 #After last "$PWD", the parameters are:
 #numExecutions numThreads ListOfCoresWhereThreadPinning... Examples:
 
 #5 2 0 2 =
 #5 executions, 2 threads, Leading in core 0, Trailing in core 2
-#5 32 0 16 2 18 4 20 6 22 8 24 10 26 12 28 14 30 1 17 3 19 5 21 7 23 9 25 11 27 13 29 15 31 = 
+#5 32 0 16 2 18 4 20 6 22 8 24 10 26 12 28 14 30 1 17 3 19 5 21 7 23 9 25 11 27 13 29 15 31 =
 #5 executions, 32 threads, L1 core 0, T1 core 16, L2 core 2, T2 core 18, ..., L16 core 15, T16 core 31
 
 # Not replicated app with different mpi ranks (to see HT results)
-mpirun -np 1 miniXyceRHT -c "$PWD"/tests/cirHuge.net "$PWD" 5 > noRep-1rank.txt  &&
-mpirun -np 16 miniXyceRHT -c "$PWD"/tests/cirHuge.net "$PWD" 5 > noRep-16ranks.txt      &&
-mpirun -np 32 miniXyceRHT -c "$PWD"/tests/cirHuge.net "$PWD" 5 > noRep-32ranks.txt      
+mpirun -np 1 miniXyce-RHT -c "$PWD"/tests/cirBig.net "$PWD" 5 > noRep-1rank.txt  &&
+mpirun -np 16 miniXyce-RHT -c "$PWD"/tests/cirBig.net "$PWD" 5 > noRep-16ranks.txt      &&
+mpirun -np 32 miniXyce-RHT -c "$PWD"/tests/cirBig.net "$PWD" 5 > noRep-32ranks.txt
 
 # 1 rank test baselines vs Our Best Approach (NewLimit + varGrouping 8)
-mpirun -np 1 miniXyce-AC -c "$PWD"/tests/cirHuge.net "$PWD" 5 2 0 2 > rep-ac-1rank-noHT.txt  &&
-mpirun -np 1 miniXyce-AC -c "$PWD"/tests/cirHuge.net "$PWD" 5 2 0 16 > rep-ac-1rank-HT.txt  &&
+mpirun -np 1 miniXyce-AC -c "$PWD"/tests/cirBig.net "$PWD" 5 2 0 2 > rep-ac-1rank-noHT.txt  &&
+mpirun -np 1 miniXyce-AC -c "$PWD"/tests/cirBig.net "$PWD" 5 2 0 16 > rep-ac-1rank-HT.txt  &&
 
-mpirun -np 1 miniXyce-SRMT -c "$PWD"/tests/cirHuge.net "$PWD" 5 2 0 2 > rep-srmt-1rank-noHT.txt  &&
-mpirun -np 1 miniXyce-SRMT -c "$PWD"/tests/cirHuge.net "$PWD" 5 2 0 16 > rep-srmt-1rank-HT.txt  &&
+mpirun -np 1 miniXyce-SRMT -c "$PWD"/tests/cirBig.net "$PWD" 5 2 0 2 > rep-srmt-1rank-noHT.txt  &&
+mpirun -np 1 miniXyce-SRMT -c "$PWD"/tests/cirBig.net "$PWD" 5 2 0 16 > rep-srmt-1rank-HT.txt  &&
 
-mpirun -np 1 miniXyce-RHT -c "$PWD"/tests/cirHuge.net "$PWD" 5 2 0 2  > rep-RHT-1rank-noHT.txt &&
-mpirun -np 1 miniXyce-RHT -c "$PWD"/tests/cirHuge.net "$PWD" 5 2 0 16 > rep-RHT-1rank-HT.txt 
+mpirun -np 1 miniXyce-RHT -c "$PWD"/tests/cirBig.net "$PWD" 5 2 0 2  > rep-RHT-1rank-noHT.txt &&
+mpirun -np 1 miniXyce-RHT -c "$PWD"/tests/cirBig.net "$PWD" 5 2 0 16 > rep-RHT-1rank-HT.txt
 
 # 16 ranks test baselines vs Our Best Approach (NewLimit + varGrouping 8)
 
-mpirun -np 16 miniXyce-AC -c "$PWD"/tests/cirHuge.net "$PWD" 5 32 0 2 4 6 8 10 12 14 1 3 5 7 9 11 13 15 16 18 20 22 24 26 28 30 17 19 21 23 25 27 29 31 > rep-ac-16rank-noHT.txt  &&
-mpirun -np 16 miniXyce-AC -c "$PWD"/tests/cirHuge.net "$PWD" 5 32 0 16 2 18 4 20 6 22 8 24 10 26 12 28 14 30 1 17 3 19 5 21 7 23 9 25 11 27 13 29 15 31 > rep-ac-16rank-HT.txt  &&
+mpirun -np 16 miniXyce-AC -c "$PWD"/tests/cirBig.net "$PWD" 5 32 0 2 4 6 8 10 12 14 1 3 5 7 9 11 13 15 16 18 20 22 24 26 28 30 17 19 21 23 25 27 29 31 > rep-ac-16rank-noHT.txt  &&
+mpirun -np 16 miniXyce-AC -c "$PWD"/tests/cirBig.net "$PWD" 5 32 0 16 2 18 4 20 6 22 8 24 10 26 12 28 14 30 1 17 3 19 5 21 7 23 9 25 11 27 13 29 15 31 > rep-ac-16rank-HT.txt  &&
 
-mpirun -np 16 miniXyce-SRMT -c "$PWD"/tests/cirHuge.net "$PWD" 5 32 0 2 4 6 8 10 12 14 1 3 5 7 9 11 13 15 16 18 20 22 24 26 28 30 17 19 21 23 25 27 29 31 > rep-ac-16rank-noHT.txt  &&
-mpirun -np 16 miniXyce-SRMT -c "$PWD"/tests/cirHuge.net "$PWD" 5 32 0 16 2 18 4 20 6 22 8 24 10 26 12 28 14 30 1 17 3 19 5 21 7 23 9 25 11 27 13 29 15 31 > rep-ac-16rank-HT.txt  &&
+mpirun -np 16 miniXyce-SRMT -c "$PWD"/tests/cirBig.net "$PWD" 5 32 0 2 4 6 8 10 12 14 1 3 5 7 9 11 13 15 16 18 20 22 24 26 28 30 17 19 21 23 25 27 29 31 > rep-ac-16rank-noHT.txt  &&
+mpirun -np 16 miniXyce-SRMT -c "$PWD"/tests/cirBig.net "$PWD" 5 32 0 16 2 18 4 20 6 22 8 24 10 26 12 28 14 30 1 17 3 19 5 21 7 23 9 25 11 27 13 29 15 31 > rep-ac-16rank-HT.txt  &&
 
-mpirun -np 16 miniXyce-RHT -c "$PWD"/tests/cirHuge.net "$PWD" 5 32 0 2 4 6 8 10 12 14 1 3 5 7 9 11 13 15 16 18 20 22 24 26 28 30 17 19 21 23 25 27 29 31 > rep-RHT-16rank-noHT.txt  &&
-mpirun -np 16 miniXyce-RHT -c "$PWD"/tests/cirHuge.net "$PWD" 5 32 0 16 2 18 4 20 6 22 8 24 10 26 12 28 14 30 1 17 3 19 5 21 7 23 9 25 11 27 13 29 15 31 > rep-RHT-16rank-HT.txt
+mpirun -np 16 miniXyce-RHT -c "$PWD"/tests/cirBig.net "$PWD" 5 32 0 2 4 6 8 10 12 14 1 3 5 7 9 11 13 15 16 18 20 22 24 26 28 30 17 19 21 23 25 27 29 31 > rep-RHT-16rank-noHT.txt  &&
+mpirun -np 16 miniXyce-RHT -c "$PWD"/tests/cirBig.net "$PWD" 5 32 0 16 2 18 4 20 6 22 8 24 10 26 12 28 14 30 1 17 3 19 5 21 7 23 9 25 11 27 13 29 15 31 > rep-RHT-16rank-HT.txt
 
 fi
-
 
 
 
